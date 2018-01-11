76d7e047af0104448efc6bb79bde141748eadae1
aimanamir <35316284+aimanamir@users.noreply.github.com>
2018-01-11 15:42:27 +0800

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN" 
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by Area 51 Reborn"
+echo -e ""
+echo -e "Modified by Aiman Amir"
+echo -e ""
+echo -e "https://telegram.me/area51reborn"
+echo -e ""
