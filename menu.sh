f8721e0f2cbe1c85071c999d9834bdb6d78c4d23
aimanamir <35316284+aimanamir@users.noreply.github.com>
2018-01-11 15:21:47 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#Menu
+
+echo -e "================================================="
+echo -e ""  
+echo -e "> menu         : menampilkan daftar perintah"
+echo -e "> usernew      : membuat akun SSH & OpenVPN"
+echo -e "> trial        : membuat akun trial"
+echo -e "> hapus        : menghapus akun SSH & OpenVPN"
+echo -e "> cek          : cek user login"
+echo -e "> member       : daftar member SSH & OpenVPN"
+echo -e "> userlimit    : Limit login Dropbear"
+echo -e "> userlimitssh : Limit login SSHD"
+echo -e "> resvis       : restart service dropbear, webmin"
+echo -e "                 squid3, OpenVPN dan SSH"
+echo -e "> reboot       : reboot VPS"
+echo -e "> speedtest    : speedtest VPS"
+echo -e "> info         : menampilkan informasi sistem"
+echo -e "> about        : info script auto install"
+echo -e "> exit         : keluar dari terminal SSH"
+echo -e ""
+echo -e "================================================="
